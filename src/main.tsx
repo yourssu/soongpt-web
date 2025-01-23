2aa73fbe58af96310411ea4b9c1245c76ba86944
2wndrhs <2wndrhs@gmail.com>
2025-01-23 17:33:03 +0900

    feat: @tanstack/react-query 의존성 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
-createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
 );
