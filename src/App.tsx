ac3cb269e6754b24f877e5e9d0d27060fb4199ae
2wndrhs <2wndrhs@gmail.com>
2025-01-23 17:50:27 +0900

    feat: tailwindcss 의존성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1 className="text-3xl font-bold underline">숭피티가 짜주는 시간표</h1>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import './index.css';
 
 const queryClient = new QueryClient();
 
