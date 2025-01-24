f4e8a496541118b4f3488cbf912744ac154ece2d
2wndrhs <2wndrhs@gmail.com>
2025-01-25 03:22:14 +0900

    feat: Pretendard 웹 폰트 설정

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Stack } from './stackflow';
+
+const App = () => {
+  return (
+    <main className="font-pretendard">
+      <Stack />
+    </main>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import '@stackflow/plugin-basic-ui/index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 import './index.css';
-import { Stack } from './stackflow';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 );
