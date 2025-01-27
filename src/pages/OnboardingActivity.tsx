efe0b1648f8b53695490b6b512c05610704c3d9a
2wndrhs <2wndrhs@gmail.com>
2025-01-28 05:02:20 +0900

    feat: 학과 입력 컴포넌트 및 상태 머신 추가

--- src/pages/OnboardingActivity.tsx
+++ src/pages/OnboardingActivity.tsx
@@ -1,16 +1,28 @@
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { ActivityComponentType } from '@stackflow/react';
 
+import { useMachine } from '@xstate/react';
 import AppBar from '../components/AppBar';
+import DepartmentInput from '../components/DepartmentInput';
+import studentMachine from '../machines/studentMachine';
 
 const OnboardingActivity: ActivityComponentType = () => {
+  const [state, send] = useMachine(studentMachine);
+
+  console.log(state);
+
   return (
     <AppScreen>
       <div className="min-h-screen py-12">
         <AppBar progress={100} />
-        <div className="mt-10 flex flex-col items-center">
+        <div className="mt-15 flex flex-col items-center">
           <h2 className="text-[28px] font-semibold">사용자님에 대해 알려주세요!</h2>
           <span className="mt-2 font-light">시간표를 만들기 위한 최소한의 정보가 필요해요.</span>
+          <div className="mt-15 w-full px-12">
+            <DepartmentInput
+              onNext={(department) => send({ type: '학과입력완료', payload: { department } })}
+            />
+          </div>
         </div>
       </div>
     </AppScreen>
