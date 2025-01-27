2b1ed19359f4f5a279b0bdbcecb5d74e81553166
2wndrhs <2wndrhs@gmail.com>
2025-01-28 02:36:25 +0900

    feat: ProgressBar 컴포넌트 구현

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,16 @@
+interface ProgressBarProps {
+  width: number; // 0 to 100
+}
+
+const ProgressBar = ({ width }: ProgressBarProps) => {
+  return (
+    <div className="h-1 w-full bg-black">
+      <div
+        className="bg-progress-bar h-full transition-all duration-500"
+        style={{ width: `${width}%` }}
+      />
+    </div>
+  );
+};
+
+export default ProgressBar;
