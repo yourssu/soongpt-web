bf4fb2ddba1c712339849eb99eb1dd2566923424
2wndrhs <2wndrhs@gmail.com>
2025-01-29 02:50:12 +0900

    feat: 진행 상태에 따라 ProgressBar가 변경되는 기능 구현

--- src/components/ChapelInput.tsx
+++ src/components/ChapelInput.tsx
@@ -2,8 +2,17 @@ import { CircleCheck } from 'lucide-react';
 import { motion } from 'motion/react';
 import { useState } from 'react';
 
-const ChapelInput = () => {
+interface ChapelInputProps {
+  onNext: (chapel: boolean) => void;
+}
+
+const ChapelInput = ({ onNext }: ChapelInputProps) => {
   const [chapel, setChapel] = useState(true);
+
+  const handleClickChapel = () => {
+    setChapel(!chapel);
+    onNext(!chapel);
+  };
 
   return (
     <motion.div
@@ -20,7 +29,7 @@ const ChapelInput = () => {
       <label className="mb-1.5 block text-sm">채플 수강 여부</label>
       <div
         className="bg-basic-light focus-visible:ring-ring flex w-full items-center justify-between rounded-lg px-4 py-3 focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:outline-none"
-        onClick={() => setChapel(!chapel)}
+        onClick={handleClickChapel}
       >
         <button type="button" className="text-primary text-lg font-semibold">
           {chapel ? '채플 수강' : '채플 미수강'}
