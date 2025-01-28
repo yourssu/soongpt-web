dedaa066972668d8f0b8c08c9ef405fb3a51c865
2wndrhs <2wndrhs@gmail.com>
2025-01-29 02:50:12 +0900

    feat: 진행 상태에 따라 ProgressBar가 변경되는 기능 구현

--- src/pages/OnboardingActivity.tsx
+++ src/pages/OnboardingActivity.tsx
@@ -2,6 +2,7 @@ import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { ActivityComponentType } from '@stackflow/react';
 
 import { useMachine } from '@xstate/react';
+import { useEffect, useState } from 'react';
 import AdmissionYearInput from '../components/AdmissionYearInput';
 import AppBar from '../components/AppBar';
 import ChapelInput from '../components/ChapelInput';
@@ -11,16 +12,37 @@ import { default as studentMachine } from '../machines/studentMachine';
 
 const OnboardingActivity: ActivityComponentType = () => {
   const [state, send] = useMachine(studentMachine);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const stateProgressMap = {
+      학과입력: 25,
+      입학년도입력: 50,
+      학년입력: 75,
+      채플수강여부입력: 100,
+    };
+
+    setProgress(stateProgressMap[state.value]);
+  }, [state.value]);
 
   return (
     <AppScreen>
       <div className="min-h-screen py-12">
-        <AppBar progress={100} />
+        <AppBar progress={progress} />
         <div className="mt-15 flex flex-col items-center">
           <h2 className="text-[28px] font-semibold">사용자님에 대해 알려주세요!</h2>
           <span className="mt-1 font-light">시간표를 만들기 위한 최소한의 정보가 필요해요.</span>
           <div className="mt-12 flex w-full flex-col gap-6 px-12">
-            {state.matches('채플수강여부입력') && <ChapelInput />}
+            {state.matches('채플수강여부입력') && (
+              <ChapelInput
+                onNext={(chapel) =>
+                  send({
+                    type: '채플수강여부입력완료',
+                    payload: { chapel },
+                  })
+                }
+              />
+            )}
 
             {(state.matches('학년입력') || state.matches('채플수강여부입력')) && (
               <GradeInput
@@ -50,6 +72,7 @@ const OnboardingActivity: ActivityComponentType = () => {
               onNext={(department) => send({ type: '학과입력완료', payload: { department } })}
             />
           </div>
+
           {state.matches('채플수강여부입력') && (
             <button
               type="button"
