57a95ab4255fe973776202d987dc231f63dfcb88
2wndrhs <2wndrhs@gmail.com>
2025-01-29 01:49:25 +0900

    feat: 입학년도 입력 컴포넌트 추가

---
+++ src/components/AdmissionYearInput.tsx
@@ -0,0 +1,103 @@
+import { Check, ChevronDown } from 'lucide-react';
+import { AnimatePresence, motion } from 'motion/react';
+import { useEffect, useRef, useState } from 'react';
+
+const admissionYears = [25, 24, 23, 22, 21, 20, 19, 18];
+
+interface AdmissionYearInputProps {
+  onNext: (admissionYear: number) => void;
+}
+
+const AdmissionYearInput = ({ onNext }: AdmissionYearInputProps) => {
+  const [admissionYear, setAdmissionYear] = useState<number>();
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showLabel, setShowLabel] = useState(false);
+  const dropDownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropDownRef.current && !dropDownRef.current.contains(event.target as Node)) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  });
+
+  const handleAdmissionYearSelect = (year: number) => {
+    setAdmissionYear(year);
+    setShowDropdown(false);
+
+    // 라벨 보이기
+    setShowLabel(true);
+    // 다음 단계로 넘어가기
+    onNext(year);
+  };
+
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+        y: -20,
+      }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.3,
+        ease: 'easeOut',
+      }}
+    >
+      {showLabel && <label className="mb-1.5 block text-sm">입학년도(학번)</label>}
+      <div className="relative" ref={dropDownRef}>
+        <div
+          className="bg-basic-light focus-visible:ring-ring flex w-full items-center justify-between rounded-lg px-4 py-3 focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:outline-none"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          <span
+            className={`text-lg font-semibold ${admissionYear ? 'text-primary' : 'text-placeholder'}`}
+          >
+            {admissionYear ?? '입학년도(학번)'}
+          </span>
+          <ChevronDown className="size-4" />
+        </div>
+        <AnimatePresence>
+          {showDropdown && (
+            <motion.ul
+              className="bg-basic-light absolute z-10 mt-2 max-h-44 w-full overflow-y-auto rounded-lg border border-gray-200 shadow-sm"
+              initial={{ opacity: 0, y: -10 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+              }}
+              exit={{
+                opacity: 0,
+                y: -10,
+              }}
+              transition={{
+                duration: 0.2,
+              }}
+            >
+              {admissionYears.map((year) => (
+                <li key={year}>
+                  <button
+                    className="text-list flex w-full items-center justify-between rounded-lg px-4 py-2 text-lg font-semibold hover:bg-gray-100"
+                    onClick={() => {
+                      handleAdmissionYearSelect(year);
+                    }}
+                  >
+                    {year}
+                    {admissionYear === year && <Check className="size-4 text-green-500" />}
+                  </button>
+                </li>
+              ))}
+            </motion.ul>
+          )}
+        </AnimatePresence>
+      </div>
+    </motion.div>
+  );
+};
+
+export default AdmissionYearInput;
--- src/pages/OnboardingActivity.tsx
+++ src/pages/OnboardingActivity.tsx
@@ -2,14 +2,13 @@ import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { ActivityComponentType } from '@stackflow/react';
 
 import { useMachine } from '@xstate/react';
+import AdmissionYearInput from '../components/AdmissionYearInput';
 import AppBar from '../components/AppBar';
 import DepartmentInput from '../components/DepartmentInput';
-import studentMachine from '../machines/studentMachine';
+import { default as studentMachine } from '../machines/studentMachine';
 
 const OnboardingActivity: ActivityComponentType = () => {
   const [state, send] = useMachine(studentMachine);
-
-  console.log(state);
 
   return (
     <AppScreen>
@@ -18,7 +17,20 @@ const OnboardingActivity: ActivityComponentType = () => {
         <div className="mt-15 flex flex-col items-center">
           <h2 className="text-[28px] font-semibold">사용자님에 대해 알려주세요!</h2>
           <span className="mt-2 font-light">시간표를 만들기 위한 최소한의 정보가 필요해요.</span>
-          <div className="mt-15 w-full px-12">
+          <div className="mt-15 flex w-full flex-col gap-6 px-12">
+            {(state.matches('입학년도입력') ||
+              state.matches('학년입력') ||
+              state.matches('채플수강여부입력')) && (
+              <AdmissionYearInput
+                onNext={(admissionYear) =>
+                  send({
+                    type: '입학년도입력완료',
+                    payload: { admissionYear },
+                  })
+                }
+              />
+            )}
+
             <DepartmentInput
               onNext={(department) => send({ type: '학과입력완료', payload: { department } })}
             />
