8e15679c9ee7ae6a788d9fd994dac62560259da8
2wndrhs <2wndrhs@gmail.com>
2025-01-31 20:25:39 +0900

    fix: 모든 상태에서 context를 업데이트 할 수 있도록 수정

--- src/machines/studentMachine.ts
+++ src/machines/studentMachine.ts
@@ -2,12 +2,12 @@ import { assign, setup } from 'xstate';
 
 export type Grade = 1 | 2 | 3 | 4 | 5;
 
-interface Context {
+type Context = {
   department: string; // 학과
   admissionYear: number; // 입학년도
   grade: Grade; // 학년
   chapel: boolean; // 채플 수강 여부
-}
+};
 
 type Event =
   | { type: '학과입력완료'; payload: { department: string } }
@@ -27,7 +27,29 @@ const studentMachine = setup({
     department: '',
     admissionYear: 0,
     grade: 1,
-    chapel: false,
+    chapel: true,
+  },
+  on: {
+    학과입력완료: {
+      actions: assign({
+        department: ({ event }) => event.payload.department,
+      }),
+    },
+    입학년도입력완료: {
+      actions: assign({
+        admissionYear: ({ event }) => event.payload.admissionYear,
+      }),
+    },
+    학년입력완료: {
+      actions: assign({
+        grade: ({ event }) => event.payload.grade,
+      }),
+    },
+    채플수강여부입력완료: {
+      actions: assign({
+        chapel: ({ event }) => event.payload.chapel,
+      }),
+    },
   },
   states: {
     학과입력: {
