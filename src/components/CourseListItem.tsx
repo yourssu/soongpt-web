a5df2e090890f04b1641b0942abf1c4813bd7df1
owl1753 <bighammer1753@gmail.com>
2025-02-05 03:08:30 +0900

    feat: CourseListItem에 애니메이션 추가

--- src/components/CourseListItem.tsx
+++ src/components/CourseListItem.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react';
 import { Course } from '../type/course.type.ts';
 
 interface CourseListItemProps {
@@ -12,9 +13,16 @@ const CourseListItem = ({ onClickCourseItem, course, isSelected }: CourseListIte
   };
 
   return (
-    <div
+    <motion.div
       onClick={handleClick}
-      className={`flex min-h-[72px] w-full items-center justify-between rounded-xl border-2 ${isSelected ? 'border-[#6B5CFF] text-[#6B5CFF]' : 'border-transparent'} bg-[#F7F8F8] px-5`}
+      initial={{ borderColor: 'rgba(107, 92, 255, 0)' }}
+      animate={{
+        borderColor: isSelected ? 'rgba(107, 92, 255, 1)' : 'rgba(107, 92, 255, 0)',
+      }}
+      transition={{ duration: 0.1 }} // 더 빠른 반응을 위해 변경
+      className={`flex min-h-[72px] w-full items-center justify-between rounded-xl border-2 bg-[#F7F8F8] px-5 ${
+        isSelected ? 'text-[#6B5CFF]' : ''
+      }`}
     >
       <div>
         <div className="text-[20px] font-semibold">{course.name}</div>
@@ -23,7 +31,7 @@ const CourseListItem = ({ onClickCourseItem, course, isSelected }: CourseListIte
       <div className="flex h-6 items-center rounded-lg bg-[#ECEFFF] px-1.5 text-[12px]/[18px] font-semibold text-[#5736F5]">
         {course.credit}학점
       </div>
-    </div>
+    </motion.div>
   );
 };
 
