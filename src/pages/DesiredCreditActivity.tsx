d1974b0c4bbc7086c7276aa221b939e04f9b5b7c
2wndrhs <2wndrhs@gmail.com>
2025-02-06 04:08:10 +0900

    refactor: 희망 학점 선택 메뉴를 `Popover` 컴포넌트로 교체

--- src/pages/DesiredCreditActivity.tsx
+++ src/pages/DesiredCreditActivity.tsx
@@ -1,12 +1,12 @@
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { ActivityComponentType } from '@stackflow/react';
 
+import * as Popover from '@radix-ui/react-popover';
 import { Check, ChevronDown } from 'lucide-react';
 import { AnimatePresence, motion } from 'motion/react';
 import { useState } from 'react';
 import AppBar from '../components/AppBar';
 import RollingNumber from '../components/RollingNumber';
-import { useDropdown } from '../hooks/useDropDown';
 
 interface DesiredCreditParams {
   majorRequired: number;
@@ -24,12 +24,12 @@ const Classification = {
 const MAX_CREDIT = 22;
 
 const DesiredCreditActivity: ActivityComponentType<DesiredCreditParams> = ({ params: credit }) => {
-  const totalCredit = Object.values(credit).reduce((acc, credit) => acc + credit, 0);
-  const availableCredits = Array.from({ length: MAX_CREDIT - totalCredit + 1 }, (_, i) => i);
+  const totalCredit = Object.values(credit).reduce((acc, credit) => acc + credit, 0); // 전공필수 + 전공선택 + 교양필수
+  const availableCredits = Array.from({ length: MAX_CREDIT - totalCredit + 1 }, (_, i) => i); // 수강 가능한 교양선택 학점
 
   const [desiredCredit, setDesiredCredit] = useState(totalCredit);
   const [generalElective, setGeneralElective] = useState(0);
-  const [showDropdown, setShowDropdown, dropDownRef] = useDropdown();
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const handleCreditSelect = (credit: number) => {
     setGeneralElective(credit);
@@ -64,56 +64,58 @@ const DesiredCreditActivity: ActivityComponentType<DesiredCreditParams> = ({ par
             ))}
             <div>
               <label className="mb-1.5 block text-sm">교양선택 학점</label>
-              <div className="relative" ref={dropDownRef}>
-                <div
-                  className="bg-basic-light flex w-full items-center justify-between rounded-xl px-4 py-3"
-                  onClick={() => setShowDropdown(!showDropdown)}
-                >
+
+              <Popover.Root open={showDropdown} onOpenChange={setShowDropdown}>
+                <Popover.Trigger asChild>
                   <button
                     type="button"
-                    className={`text-lg font-semibold ${generalElective === 0 ? 'text-placeholder' : 'text-primary'}`}
+                    className={`bg-basic-light focus-visible:outline-ring flex w-full items-center justify-between rounded-xl px-4 py-3 text-lg font-semibold ${generalElective === 0 ? 'text-placeholder' : 'text-primary'}`}
                   >
                     {generalElective}
+                    <ChevronDown className="text-text size-4" />
                   </button>
-                  <ChevronDown className="size-4" />
-                </div>
+                </Popover.Trigger>
+
                 <AnimatePresence>
                   {showDropdown && (
-                    <motion.ul
-                      className="bg-basic-light absolute z-10 mt-2 max-h-44 w-full overflow-y-auto rounded-xl border border-gray-200 shadow-sm"
-                      initial={{ opacity: 0, y: -10 }}
-                      animate={{
-                        opacity: 1,
-                        y: 0,
-                      }}
-                      exit={{
-                        opacity: 0,
-                        y: -10,
-                      }}
-                      transition={{
-                        duration: 0.2,
-                      }}
-                    >
-                      {availableCredits.map((availableCredit) => (
-                        <li key={availableCredit}>
-                          <button
-                            type="button"
-                            className="text-list flex w-full items-center justify-between rounded-xl px-4 py-2 text-lg font-semibold hover:bg-gray-100"
-                            onClick={() => handleCreditSelect(availableCredit)}
-                          >
-                            {availableCredit}
-                            {availableCredit === generalElective && (
-                              <Check className="size-4 text-green-500" />
-                            )}
-                          </button>
-                        </li>
-                      ))}
-                    </motion.ul>
+                    <Popover.Content asChild sideOffset={5} forceMount>
+                      <motion.ul
+                        className="bg-basic-light z-10 max-h-44 w-[var(--radix-popover-trigger-width)] overflow-y-auto rounded-xl border border-gray-200 shadow-sm"
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{
+                          opacity: 1,
+                          y: 0,
+                        }}
+                        exit={{
+                          opacity: 0,
+                          y: -10,
+                        }}
+                        transition={{
+                          duration: 0.2,
+                        }}
+                      >
+                        {availableCredits.map((availableCredit) => (
+                          <li key={availableCredit}>
+                            <button
+                              type="button"
+                              className="text-list focus-visible:outline-ring flex w-full items-center justify-between rounded-xl px-4 py-2 text-lg font-semibold hover:bg-gray-100 focus-visible:-outline-offset-1"
+                              onClick={() => handleCreditSelect(availableCredit)}
+                            >
+                              {availableCredit}
+                              {availableCredit === generalElective && (
+                                <Check className="size-4 text-green-500" />
+                              )}
+                            </button>
+                          </li>
+                        ))}
+                      </motion.ul>
+                    </Popover.Content>
                   )}
                 </AnimatePresence>
-              </div>
+              </Popover.Root>
             </div>
           </div>
+
           {generalElective > 0 && (
             <motion.button
               type="button"
