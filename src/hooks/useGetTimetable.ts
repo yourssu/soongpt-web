dcae08875b281327c3d7c84102f914845cdd8308
2wndrhs <2wndrhs@gmail.com>
2025-02-08 04:13:36 +0900

    fix: timetable 스키마에 `score` 필드 추가

--- src/hooks/useGetTimetable.ts
+++ src/hooks/useGetTimetable.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import api from '../api/client';
-import { timetableResponseSchema } from '../schemas/timeTableSchema';
+import { timetableResponseSchema } from '../schemas/timetableSchema';
 
 export const useGetTimetable = (timetableId: number) => {
   return useQuery({
