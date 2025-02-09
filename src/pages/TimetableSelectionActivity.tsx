9c40bc5b6efaa4a7a1531897904e876b1d43ef93
2wndrhs <2wndrhs@gmail.com>
2025-02-09 22:09:27 +0900

    feat: 시간표 추천 API 연결

--- src/pages/TimetableSelectionActivity.tsx
+++ src/pages/TimetableSelectionActivity.tsx
@@ -1,12 +1,14 @@
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { ActivityComponentType } from '@stackflow/react';
 
+import { MutationState, useMutationState } from '@tanstack/react-query';
 import useEmblaCarousel from 'embla-carousel-react';
 import { useCallback, useEffect, useState } from 'react';
 import AppBar from '../components/AppBar';
 import Timetable from '../components/Timetable';
 import { TimetableSkeleton } from '../components/TimetableSkeleton';
-import { Timetable as TimetableType } from '../schemas/timetableSchema';
+import { TimetableArrayResponse, Timetable as TimetableType } from '../schemas/timetableSchema';
+import { useFlow } from '../stackflow';
 
 export const mockTimetable: TimetableType = {
   timetableId: 1,
@@ -131,18 +133,18 @@ export const mockTimetable: TimetableType = {
 };
 
 const TimetableSelectionActivity: ActivityComponentType = () => {
-  // const timetableMutation = useMutationState({
-  //   filters: { mutationKey: ['timetables'] },
-  // });
+  const timetableMutation = useMutationState<MutationState<TimetableArrayResponse>>({
+    filters: { mutationKey: ['timetables'] },
+  });
 
-  // console.log(timetableMutation);
-
-  const [isPending, setIsPending] = useState(true);
+  const latestMutation = timetableMutation[timetableMutation.length - 1];
 
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [emblaRef, emblaApi] = useEmblaCarousel({
     axis: 'y',
   });
+
+  const { push } = useFlow();
 
   const onSelect = useCallback(() => {
     if (emblaApi) {
@@ -161,8 +163,7 @@ const TimetableSelectionActivity: ActivityComponentType = () => {
   }, [emblaApi, onSelect]);
 
   const handleNextClick = () => {
-    setIsPending(!isPending);
-    // push('TimetableSharingActivity', {});
+    push('TimetableSharingActivity', {});
   };
 
   return (
@@ -173,19 +174,22 @@ const TimetableSelectionActivity: ActivityComponentType = () => {
           <h2 className="text-center text-[28px] font-semibold">
             사용자님을 위한
             <br />
-            시간표를 가져왔어요!
+            시간표를 {latestMutation.status === 'pending' ? '가져오는 중이에요!' : '가져왔어요!'}
           </h2>
           <div className="mt-8 w-full flex-1 overflow-hidden px-10" ref={emblaRef}>
             <div className="-mt-4 flex flex-col" style={{ maxHeight: 'calc(100vh - 300px)' }}>
-              {isPending ? (
+              {latestMutation.status === 'pending' ? (
                 <TimetableSkeleton className="pt-4">
                   <TimetableSkeleton.Header />
                 </TimetableSkeleton>
               ) : (
-                [0, 1, 2].map((index) => (
-                  <div key={index} className={`min-h-0 flex-shrink-0 transform-gpu pt-4`}>
+                latestMutation.data?.result.timetables.map((timetable, index) => (
+                  <div
+                    key={timetable.timetableId}
+                    className={`min-h-0 flex-shrink-0 transform-gpu pt-4`}
+                  >
                     <Timetable
-                      timetable={mockTimetable}
+                      timetable={timetable}
                       className={`${index === selectedIndex ? 'border-primary' : 'border-placeholder'}`}
                     >
                       <Timetable.Header
@@ -199,7 +203,8 @@ const TimetableSelectionActivity: ActivityComponentType = () => {
           </div>
           <button
             type="button"
-            className="bg-primary mt-4 w-50 rounded-2xl py-3.5 font-semibold text-white"
+            className={`mt-4 w-50 rounded-2xl py-3.5 font-semibold text-white ${latestMutation.status === 'pending' ? 'bg-gray-300' : 'bg-primary'}`}
+            disabled={latestMutation.status === 'pending'}
             onClick={handleNextClick}
           >
             이 시간표가 좋아요
