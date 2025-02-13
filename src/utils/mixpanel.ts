e080a7d0fe2c0c07c3a3c9854fe2bcb5c7c2dbbf
이중곤 <2wndrhs@gmail.com>
2025-02-13 21:24:47 +0900

    feat: Mixpanel 에러 로깅 및 세션 기록 추가 (#78)
    * feat: 시간표 선택 과정에서 에러 발생 시 Mixpanel에 에러 추적 추가
    
    * feat: Mixpanel에 세션 기록 기능 추가

--- src/utils/mixpanel.ts
+++ src/utils/mixpanel.ts
@@ -1,4 +1,5 @@
 import mixpanel from 'mixpanel-browser';
+import { SoongptError } from '../schemas/errorSchema';
 import { CoursePreference, Student } from '../schemas/studentSchema';
 import { Timetable } from '../schemas/timetableSchema';
 import { CourseType } from '../type/course.type';
@@ -11,6 +12,7 @@ mixpanel.init(MIXPANEL_TOKEN, {
   track_pageview: 'url-with-path',
   persistence: 'localStorage',
   ignore_dnt: true,
+  record_sessions_percent: 100,
 });
 
 export const Mixpanel = {
@@ -56,8 +58,10 @@ export const Mixpanel = {
     });
   },
 
-  trackTimetableSelectionError: () => {
-    mixpanel.track('Timetable Selection Error');
+  trackTimetableSelectionError: (error: SoongptError) => {
+    mixpanel.track('Timetable Selection Error', {
+      error,
+    });
   },
 
   trackTimetableSharingEnter: (timetable: Timetable) => {
