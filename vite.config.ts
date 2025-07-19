8621352cfd17ef98ee1d03f6851b0ce32e8b1e34
fecapark <fecapark@gmail.com>
2025-07-19 22:50:48 +0900

    chore: import alias @ 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     react(),
     tailwindcss(),
     sentryVitePlugin({
