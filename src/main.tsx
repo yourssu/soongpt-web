8544422dd2779c4bbaaa49ff08a0ec5fb8ce7da7
fecapark <fecapark@gmail.com>
2025-07-21 21:07:56 +0900

    refactor: Provider 컴포넌트들 그룹화 및 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import '@stackflow/plugin-basic-ui/index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -9,13 +8,15 @@ import './index.css';
 import './bootstrap/channelTalk';
 import './bootstrap/mixpanel';
 import './bootstrap/sentry';
-
-const queryClient = new QueryClient();
+import { TanstackQueryProvider } from '@/components/Providers/TanstackQueryProvider';
+import { StudentMachineContext } from '@/contexts/StudentMachineContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <TanstackQueryProvider>
+      <StudentMachineContext.Provider>
+        <App />
+      </StudentMachineContext.Provider>
+    </TanstackQueryProvider>
   </StrictMode>,
 );
