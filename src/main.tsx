4665fb828edc511ca6ca4d9caa8686b0cfce3ec6
fecapark <fecapark@gmail.com>
2025-07-21 21:39:52 +0900

    feat: ToastProvider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,22 @@ import '@stackflow/plugin-basic-ui/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from '@/App';
-
 import './index.css';
 import './bootstrap/channelTalk';
 import './bootstrap/mixpanel';
 import './bootstrap/sentry';
+import App from '@/App';
 import { TanstackQueryProvider } from '@/components/Providers/TanstackQueryProvider';
+import { ToastProvider } from '@/components/Providers/ToastProvider';
 import { StudentMachineContext } from '@/contexts/StudentMachineContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TanstackQueryProvider>
       <StudentMachineContext.Provider>
-        <App />
+        <ToastProvider duration={3000}>
+          <App />
+        </ToastProvider>
       </StudentMachineContext.Provider>
     </TanstackQueryProvider>
   </StrictMode>,
