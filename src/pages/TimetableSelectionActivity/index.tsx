f2bd270b0b3160df9d44d61b8f4a8cdfb7f3e4be
fecapark <fecapark@gmail.com>
2025-08-01 03:18:42 +0900

    feat: 시간표 페이지 > 장바구니 안내 다이얼로그 추가

--- src/pages/TimetableSelectionActivity/index.tsx
+++ src/pages/TimetableSelectionActivity/index.tsx
@@ -7,6 +7,7 @@ import { Mixpanel } from '@/bootstrap/mixpanel';
 import { ActivityLayout } from '@/components/ActivityLayout';
 import { ProgressAppBar } from '@/components/AppBar/ProgressAppBar';
 import Timetable from '@/components/Timetable';
+import { useAlertDialog } from '@/hooks/useAlertDialog';
 import { TimetableSkeleton } from '@/pages/TimetableSharingActivity/components/TimetableSkeleton';
 import { SoongptError } from '@/schemas/errorSchema';
 import { TimetableArrayResponse } from '@/schemas/timetableSchema';
@@ -21,6 +22,7 @@ interface TimetableSelection {
 type TimetableMutationStatus = 'error400' | 'error500' | Exclude<MutationStatus, 'error'>;
 
 const TimetableSelectionActivity: ActivityComponentType = () => {
+  const openShoppingCartDialog = useAlertDialog();
   const timetableMutation = useMutationState<MutationState<TimetableArrayResponse, SoongptError>>({
     filters: { mutationKey: ['timetables'] },
   });
@@ -142,6 +144,37 @@ const TimetableSelectionActivity: ActivityComponentType = () => {
     },
   };
 
+  const handleShoppingCartButtonClick = () => {
+    openShoppingCartDialog({
+      title: '예비 수강신청(장바구니)',
+      closeButton: true,
+      closeableWithOutside: true,
+      content: (
+        <div className="font-medium">
+          <div>[장바구니 이용 기간]</div>
+          <div>
+            <span className="text-brandSecondary">7/30(수) 00:00</span> ~ 9/5(금) 09:00
+          </div>
+          <br />
+          <div>[장바구니 이용 제한 기간]</div>
+          <div>
+            <span className="text-brandSecondary">8/4(월) ~ 8/8(금) 09:00 ~ 16:00은 이용 불가</span>
+          </div>
+          <div>(수강신청 변경 기간도 이용 불가)</div>
+          <br />
+          <div>
+            - <span className="text-brandSecondary">최대 15과목</span>까지 담기 가능
+          </div>
+          <br />
+          <div>
+            - 장바구니에 담아놓아도{' '}
+            <span className="text-brandSecondary">자동 수강신청 되지 않음</span>
+          </div>
+        </div>
+      ),
+    });
+  };
+
   if (!latestMutation) {
     return null;
   }
@@ -162,7 +195,13 @@ const TimetableSelectionActivity: ActivityComponentType = () => {
               <h2 className="mt-6 text-center text-[28px]/[normal] font-semibold whitespace-pre-wrap">
                 {timetableSelection[mutationStatus].title}
               </h2>
-              <span className="font-light">원하는 시간표를 장바구니에 담아보세요</span>
+              <span className="font-light">원하는 시간표를 장바구니에 담아보세요.</span>
+              <button
+                className="text-brandPrimary text-xs underline"
+                onClick={handleShoppingCartButtonClick}
+              >
+                예비 수강신청(장바구니) 안내
+              </button>
             </div>
             <div className="mt-7.5 flex w-full flex-1 flex-col pb-4">
               {timetableSelection[mutationStatus].element()}
