4efd11a2ee552028072887625abefe7eac6215ae
fecapark <fecapark@gmail.com>
2025-08-05 01:39:47 +0900

    feat: 액티비티 컴포넌트 맵 작성

---
+++ src/pages/routes.ts
@@ -0,0 +1,32 @@
+import { lazy } from '@stackflow/react/future';
+
+import { ActivityName } from '@/stackflow/Activity';
+import { ActivityComponentType } from '@/utils/stackflow';
+
+export const routes: Record<ActivityName, ActivityComponentType<any>> = {
+  onboarding: lazy(() =>
+    import('@/pages/OnboardingActivity').then((module) => ({
+      default: module.OnboardingActivity,
+    })),
+  ),
+  course_selection: lazy(() =>
+    import('@/pages/CourseSelectionActivity').then((module) => ({
+      default: module.CourseSelectionActivity,
+    })),
+  ),
+  desired_credit: lazy(() =>
+    import('@/pages/DesiredCreditActivity').then((module) => ({
+      default: module.DesiredCreditActivity,
+    })),
+  ),
+  timetable_selection: lazy(() =>
+    import('@/pages/TimetableSelectionActivity').then((module) => ({
+      default: module.TimetableSelectionActivity,
+    })),
+  ),
+  course_search: lazy(() =>
+    import('@/pages/CourseSearchActivity').then((module) => ({
+      default: module.CourseSearchActivity,
+    })),
+  ),
+};
