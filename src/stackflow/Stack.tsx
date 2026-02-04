ef44320f9ad6b09f1b417706e934ab85f59a1841
PeraSite <pretocki3@gmail.com>
2026-02-04 21:51:48 +0900

    feat: UTM 유실 해결, alpha 환경 API URL dev25-2로 변경

--- src/stackflow/Stack.tsx
+++ src/stackflow/Stack.tsx
@@ -5,6 +5,7 @@ import { stackflow } from '@stackflow/react/future';
 
 import { routes } from '@/pages/routes';
 import { stackflowConfig } from '@/stackflow/config';
+import { utmPreservePlugin } from '@/stackflow/plugins/utmPreservePlugin.ts';
 
 export const { Stack } = stackflow({
   config: stackflowConfig,
@@ -18,5 +19,11 @@ export const { Stack } = stackflow({
       config: stackflowConfig,
       fallbackActivity: () => 'onboarding',
     }),
+
+    /**
+     * historySyncPlugin이 먼저 initialEvents를 생성해야 최초 접속 시 UTM을 주입할 수 있으므로
+     * historySyncPlugizn 이후에 utmPreservePlugin을 등록해야 함
+     */
+    utmPreservePlugin(),
   ],
 });
