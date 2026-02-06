abc37e87e1704e1a8e75cceacaee2b5d449ccaed
PeraSite <pretocki3@gmail.com>
2026-02-06 16:13:33 +0900

    feat: 모든 과목 이수 완료 컴포넌트 디자인 개선

--- src/pages/CourseSelectionActivity/components/CourseSelectionSteps/CourseBySelectedGradesEmpty.tsx
+++ src/pages/CourseSelectionActivity/components/CourseSelectionSteps/CourseBySelectedGradesEmpty.tsx
@@ -1,6 +1,6 @@
 export const CourseBySelectedGradesEmpty = () => {
   return (
-    <div className="flex flex-1 flex-col bg-white">
+    <div className="flex flex-1 flex-col rounded-4xl bg-white">
       <div className="flex w-full flex-1 place-items-center">
         <div className="flex w-full flex-col place-items-center gap-4">
           <span className="text-[20px] font-medium tracking-[-0.4px] text-[#acacac]">
